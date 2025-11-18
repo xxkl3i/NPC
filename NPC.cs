371801cc4650d1e7899c07d8a1b25547b42e354b
xxkl3i <162039217+xxkl3i@users.noreply.github.com>
2025-11-18 20:00:39 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public int health = 5;
+
+    public int level = 1;
+
+    public float speed = 1.2f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
